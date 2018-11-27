076d2d4bb1bad8d2d93116d4bc7b943f60f98899
Iker Suen <apple@appledeMacBook-Air-2.local>
2018-11-28 00:26:36 +0800

    (iker) implemented Employee and EmployeeService, and GET POST PUT

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -0,0 +1,46 @@
+package com.tw.apistackbase.controller;
+
+import java.util.List;
+
+import com.tw.apistackbase.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeResource {
+
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(produces = {"application/json"})
+    public ResponseEntity<List<Employee>> getEmployee() {
+        List<Employee> employees = employeeService.getAll();
+        return ResponseEntity.ok(employees);
+    }
+
+    @PostMapping(produces = "application/json")
+    public int addEmployee(@RequestBody Employee employee){
+        int id = employeeService.add(employee);
+        return id;
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody String updateEmployee(@PathVariable int id, @RequestBody Employee employeeUpdate){
+        if(employeeService.findById(id) != null){
+            Employee update = employeeService.findById(id);
+            update.setAge(employeeUpdate.getAge());
+            update.setGender(employeeUpdate.getGender());
+            update.setName(employeeUpdate.getName());
+            return "update success";
+        }else{
+            return "update failed";
+        }
+    }
+}
---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private List<Employee> employees;
+    private int idIndex = 0;
+
+    public EmployeeService(){
+        this.employees = new ArrayList<>();
+    }
+
+    public List<Employee> getAll() {
+        return this.employees;
+    }
+
+    public int add(Employee employee){
+        employee.setId(idIndex++);
+        this.employees.add(employee);
+        return idIndex;
+    }
+
+    public Employee findById(int id){
+        for(Employee employee : this.employees){
+            if(employee.getId() == (id)){
+                return employee;
+            }
+        }
+        return null;
+    }
+}
+
